e667558e50b72b835e76f377287dfdc90ce94007
dhruvdwivedi005 <dhruvdwivedi005@gmail.com>
2024-09-14 17:56:12 +0530

    1st backend Itteration

---
+++ backend/back2/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import shop
+from .serializers import UserSerializer
+
+# Create your views here.
+
+@api_view(['GET'])
+def get_shop(request):
+    users= shop.objects.all()
+    serializer= UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_shop(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
